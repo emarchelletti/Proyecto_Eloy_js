e1c16e459655541d80526f103ba7d6329520eacd
Eloy <eloymarchelletti@gmail.com>
2023-06-08 20:31:09 -0300

    Modificaciones antes de la entrega 1

--- js/App.js
+++ js/App.js
@@ -6,35 +6,37 @@ let precioTotal;
 
 //Declaro la funcion que pide al usuario confirmar si quiere hacer otra reserva
 function obtenerConfirmacion(mensaje) {
-    let respuesta = prompt(mensaje + ' (Ingrese "si" o "no")');
+    let respuesta = prompt(mensaje + ' (Si / No)');
     return respuesta.toLowerCase() === 'si';
 }
 
 alert ("Bienvenido al sistema de reservas de " + `"Epecuen Lodge"`)
 
 do {
-    let tipoCabanaInput = parseInt(prompt('Ingrese el tipo de cabaña en la que desea hospedarse:\n1. Estandar\n2. Deluxe'));
+    let tipoCabanaInput = parseInt(prompt('Ingrese el tipo de cabaña en la que desea hospedarse:\n1. Estandar\n2. Deluxe')); //3
 
     // Verificar el tipo de cabaña ingresado
     if (tipoCabanaInput === 1) {
         tipoCabana = 'Estandar';
-        cantidadPersonas = parseInt(prompt('Usted a seleccionado la cabaña "Estandar"\n\nIngrese la cantidad de personas que se hospedarán (1 a 4 personas):'));
+        cantidadPersonas = parseInt(prompt('Usted a seleccionado la cabaña "Estandar"\n\nIngrese la cantidad de huespedes (minimo 1 maximo 4):'));
         if (cantidadPersonas < 1 || cantidadPersonas > 4) {
-            alert('Cantidad de personas inválida para cabaña Estandar.\n\nInicie el proceso nuevamente');
-            break;
+            alert('Cantidad de huespedes inválida para cabaña Estandar.\n\nInicie el proceso nuevamente');
+            continue;
         }
         precioPorPersonaPorDia = 100;
+
     } else if (tipoCabanaInput === 2) {
         tipoCabana = 'Deluxe';
-        cantidadPersonas = parseInt(prompt('Ingrese la cantidad de personas que se hospedarán (2 a 8 personas):'));
+        cantidadPersonas = parseInt(prompt('Ingrese la cantidad de huespedes que se hospedarán (minimo 2 maximo 8):'));
         if (cantidadPersonas < 2 || cantidadPersonas > 8) {
             alert('Cantidad de personas inválida para cabaña Deluxe.');
-            break;
+            continue;
         }
         precioPorPersonaPorDia = 500;
+
     } else {
-        alert('Opción inválida. Por favor, ingrese 1 para cabaña Estandar o 2 para cabaña Deluxe.');
-        break;
+        alert('Opción inválida. Por favor, recuerde ingresar 1 para cabaña Estandar o 2 para cabaña Deluxe.');
+        continue;
     }
 
     cantidadDias = parseInt(prompt('Ingrese la cantidad de días de estadía (entre 5 y 15 días):'));
@@ -42,7 +44,7 @@ do {
     // Verificar la cantidad de días ingresada
     if (cantidadDias < 5 || cantidadDias > 15) {
         alert('Cantidad de días inválida.');
-        break;
+        continue
     }
 
     // Calcular el precio total
@@ -58,12 +60,8 @@ do {
 
     alert(mensaje);
 
-} while (obtenerConfirmacion('¿Desea realizar otra reserva?')); 
+} while (obtenerConfirmacion('Desea repetir el proceso de reserva?')); 
 //El while se ejecuta mientras la funcion "obtenerConfirmacion" devuelva true, lo que significa que el usuario desea realizar otra reserva. El bucle se detendrá cuando la función devuelva false, indicando que el usuario no desea realizar otra reserva.
-
-
-
-
 
 
 
